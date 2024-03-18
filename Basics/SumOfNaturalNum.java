950bf8b896b4d99acfb64cac4898b8433140c8ef
Jasika pal <jasikapal@gmail.com>
2024-03-18 15:22:38 +0530

    SumOf N natural numbers

---
+++ Basics/SumOfNaturalNum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of " + num + " natural nos. is " + sum);
+    }
+}
