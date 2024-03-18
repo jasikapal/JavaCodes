2c956ebbd31f4a567d8300c99ed2516b34c5876e
Jasika pal <jasikapal@gmail.com>
2024-03-18 18:35:27 +0530

    Multiplication

---
+++ Basics/Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number:");
+        int a=sc.nextInt();
+        System.out.println("Enter 1st number:");
+        int b=sc.nextInt();
+        int p=a*b;
+        System.out.println(p);
+}
+}
