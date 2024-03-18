6e9549cbf0f9c0cf82f88e5043e2c7a9950c75b8
Jasika pal <jasikapal@gmail.com>
2024-03-18 18:26:01 +0530

    Add

---
+++ Basics/Add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Add {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number:");
+        int a=sc.nextInt();
+        System.out.println("Enter 1st number:");
+        int b=sc.nextInt();
+        int sum= a+b;
+        System.out.println(sum);
+
+    }
+}
