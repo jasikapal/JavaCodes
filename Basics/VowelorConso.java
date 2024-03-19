d4ee049c708f466656c19d515e401e8162e8a782
Jasika pal <jasikapal@gmail.com>
2024-03-19 17:28:46 +0530

    VowelConso

---
+++ Basics/VowelorConso.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class VowelorConso {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter an alphabet:");
+    char c = sc.next().charAt(0);
+
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'||c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+      System.out.println("vowel");
+    } else {
+      System.out.println("consonent");
+    }
+
+  }
+}
