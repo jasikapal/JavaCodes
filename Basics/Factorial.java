9d9d65a4971424f9b85f7c052275a753f893c983
Jasika pal <jasikapal@gmail.com>
2024-03-19 21:45:40 +0530

    sc close

--- Basics/Factorial.java
+++ Basics/Factorial.java
@@ -4,6 +4,7 @@ public class Factorial {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter a number:");
     int a=sc.nextInt();
+    sc.close();
     int p =1;
     for(int i=1;i<=a;i++){
         p=p*i;
