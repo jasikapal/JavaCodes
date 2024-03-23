58daabaec561d2431fca1201489a649b76b34d18
Jasika pal <jasikapal@gmail.com>
2024-03-23 19:41:18 +0530

    oddnumtriangle

---
+++ PatternProgramming/OddNumTriangle.java
@@ -0,0 +1,22 @@
+package PatternProgramming;
+
+import java.util.Scanner;
+
+public class OddNumTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= num; i++) {
+            int a = 1;
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(a);
+                a = a + 2;
+
+            }
+            System.out.println();
+        }
+    }
+}
