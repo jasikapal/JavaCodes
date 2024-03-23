c5f770ebba625e00a35a48eb93cad60be37a2011
Jasika pal <jasikapal@gmail.com>
2024-03-23 19:41:45 +0530

    nothing modified

--- PatternProgramming/NumTriangle.java
+++ PatternProgramming/NumTriangle.java
@@ -12,7 +12,7 @@ public class NumTriangle {
         for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
-            }
+            }                                                                                           
             System.out.println();
         }
     }
