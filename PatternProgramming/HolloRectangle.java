5dd61116d432ff78d70f8ab329d7a56a2d979080
Jasika pal <jasikapal@gmail.com>
2024-03-25 15:01:11 +0530

    HolloRectangle

---
+++ PatternProgramming/HolloRectangle.java
@@ -0,0 +1,21 @@
+package PatternProgramming;
+import java.util.Scanner;
+public class HolloRectangle {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the no. of rows:");
+        int r = sc.nextInt();
+        System.out.println("Enter the no. of column:");
+        int c = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+                if (i==1||j==1||i==r||j==c) {
+                    System.out.print("*");
+    }
+    else{
+        System.out.print(" ");
+    }
+}System.out.println();
+        }
+    }}
