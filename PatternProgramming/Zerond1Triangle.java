e5e797a490bfcb8585ea844ea705964823869bee
Jasika pal <jasikapal@gmail.com>
2024-03-25 16:30:32 +0530

    0nd1triangle

---
+++ PatternProgramming/Zerond1Triangle.java
@@ -0,0 +1,24 @@
+package PatternProgramming;
+
+import java.util.Scanner;
+
+public class Zerond1Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+
+}
