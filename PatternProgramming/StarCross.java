f4068e13b3bae5ead78f01eb43cf3dd5cc5a4f22
Jasika pal <jasikapal@gmail.com>
2024-03-25 15:10:00 +0530

    StarCross

---
+++ PatternProgramming/StarCross.java
@@ -0,0 +1,21 @@
+package PatternProgramming;
+import java.util.Scanner;
+
+public class StarCross {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num; j++) {
+                if(i==j||i+j==num+1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+    }System.out.println();
+}
+    }
+}
+
